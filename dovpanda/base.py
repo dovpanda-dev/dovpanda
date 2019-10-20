013b667387fdec1c710972c0a13d042f12917fbc
Gal Braun <galbraun@gmail.com>
2019-10-20 10:26:27 +0300

    add option to suffix function and added a chack for duplicated indexes after concat

--- dovpanda/base.py
+++ dovpanda/base.py
@@ -5,23 +5,28 @@ from collections import defaultdict
 
 class Ledger:
     def __init__(self):
-        self.hooks = defaultdict(list)
+        self.prefix_hooks = defaultdict(list)
+        self.suffix_hooks = defaultdict(list)
 
-
-    def replace(self, original, replacement):
+    def replace(self, original, replacement, order_type='prefix'):
         g = rgetattr(sys.modules['pandas'], original)
-        rsetattr(sys.modules['pandas'], original, prefix_function(g, replacement))
+        if order_type == 'prefix':
+            rsetattr(sys.modules['pandas'], original, prefix_function(g, replacement))
+        else:
+            rsetattr(sys.modules['pandas'], original, suffix_function(g, replacement))
 
-    def add_hook(self, original):
+    def add_hook(self, original, order_type='prefix'):
         def replaces_decorator(replacement):
-            self.hooks[original].append(replacement)
+            self.prefix_hooks[original].append(replacement) if order_type == 'prefix' else self.suffix_hooks[
+                original].append(replacement)
 
         return replaces_decorator
 
     def register_hooks(self):
-        for k, v in self.hooks.items():
-            self.replace(k, v)
-
+        for k, v in self.prefix_hooks.items():
+            self.replace(k, v, 'prefix')
+        for k, v in self.suffix_hooks.items():
+            self.replace(k, v, 'suffix')
 
 
 def prefix_function(f, pres):
@@ -32,6 +37,17 @@ def prefix_function(f, pres):
         for pre in pres:
             pre(*args, **kwargs)
         return f(*args, **kwargs)
+
+    return run
+
+
+def suffix_function(f, suffixes):
+    @functools.wraps(f)
+    def run(*args, **kwargs):
+        res = f(*args, **kwargs)
+        for suffix in suffixes:
+            suffix(res, *args, **kwargs)
+        return res
 
     return run
 
@@ -54,6 +70,7 @@ def rsetattr(obj, attr, value):
 def only_print(s, *args, **kwargs):
     print(s)
     return lambda x: (args, kwargs)
+
 
 def listify(val):
     if type(val) is str:
