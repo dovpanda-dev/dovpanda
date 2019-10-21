ef11096cab4f78fdea72957fae0021cc9adbb8e6
deanla@gmail.com <deanla@gmail.com>
2019-10-21 22:26:28 +0300

    axis checks for `pd.concat`
    closes #7
    closes #8

--- dovpanda/core.py
+++ dovpanda/core.py
@@ -33,4 +33,30 @@ def time_grouping(*args, **kwargs):
 @ledger.add_hook('concat', hook_type='post')
 def duplicate_index_after_concat(res, *args, **kwargs):
     if res.index.nunique() != len(res.index):
-        ledger.tell('After concatenating you have duplicated indexes values - pay attention')
+        ledger.tell('After concatenation you have duplicated indexes values - pay attention')
+    if res.columns.nunique() != len(res.columns):
+        ledger.tell('After concatenation you have duplicated column names - pay attention')
+
+
+@ledger.add_hook('concat')
+def wrong_concat_axis(*args, **kwargs):
+    objs = base.get_arg(args, kwargs, 0, 'objs')
+    axis = base.get_arg(args, kwargs, 1, 'axis')
+    rows = {df.shape[0] for df in objs}
+    cols = {df.shape[1] for df in objs}
+    col_names = set.union(*[set(df.columns) for df in objs])
+    same_cols = (len(cols) == 1) and (len(col_names) == list(cols)[0])
+    same_rows = (len(rows) == 1)
+    axis_translation = {0: 'vertically', 1: 'horizontally'}
+    if same_cols and not same_rows:
+        if axis == 1:
+            ledger.tell("All dataframes have the same columns, which hints for concat on axis 0."
+                        "You specified <code>axis=1</code> which may result in an unwanted behaviour")
+    elif same_rows and not same_cols:
+        if axis == 0:
+            ledger.tell("All dataframes have same number of rows, which hints for concat on axis 1."
+                        "You specified <code>axis=0</code> which may result in an unwanted behaviour")
+
+    elif same_rows and same_rows:
+        ledger.tell("All dataframes have the same columns and same number of rows. "
+                    f"Pay attention, your axis is {axis} which concatenates {axis_translation[axis]}")
