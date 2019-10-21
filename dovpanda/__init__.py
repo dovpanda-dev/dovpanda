8d88c854c9d7dce6d7df894ac944146f82d03e3b
deanla@gmail.com <deanla@gmail.com>
2019-10-21 15:26:54 +0300

    framework change - change output
    also called in init

--- dovpanda/__init__.py
+++ dovpanda/__init__.py
@@ -1,10 +1,10 @@
 import sys
 
+from dovpanda.core import ledger
 if 'pandas' in sys.modules.keys():
-    from dovpanda.core import ledger
     ledger.register_hooks()
 else:
-    print('Pandas not imported')
+    ledger.tell('Pandas not imported')
 
 def set_output(tell_method):
     ledger.set_output(tell_method)
