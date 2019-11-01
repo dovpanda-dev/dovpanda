7b88a71925e1b555d93a073e638e6909697fac11
Dean Langsam <DeanLa@users.noreply.github.com>
2019-11-01 14:50:24 +0200

    Update config.py

--- dovpanda/config.py
+++ dovpanda/config.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 READ_METHODS = [method for method in dir(pd) if 'read' in method]
 DF_CREATION = READ_METHODS + ['DataFrame.__init__']
-SERIES_CREATION = READ_METHODS
+SERIES_CREATION = READ_METHODS + ['Series.__init__']
